f616282b7e192d74ed2fa89f1e77ce9e1663854b
helisson <helissonomc@dcomp.ufs.br>
2021-12-03 12:52:11 -0300

    Custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager,
+                                        PermissionsMixin)
 
-# Create your models here.
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password, user_type, **extra_fields):
+        """ Cria e salva novos user"""
+        if not email:
+            raise ValueError("Must Have Email Adress")
+
+        user = self.model(email=email.lower(), user_type=user_type,**extra_fields)
+        user.set_password(password)
+        
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password, user_type='Administrador', **extra_fields):
+        """Cria super user e atribui o grupo 'Administrador ao usuário'"""
+
+        user = self.create_user(email, password, user_type)
+        user.is_staff = True
+
+        user.is_superuser = True
+        user.save(using=self._db)
+  
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    TYPES = (
+        ('Administrador', 'Administrador'),
+        ('Anunciante', 'Anunciante'),
+    )
+
+
+    email = models.EmailField(max_length=255, unique=True)
+    user_type = models.CharField(max_length=100, choices=TYPES)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
