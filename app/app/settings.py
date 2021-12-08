6b4b3334208855ba7bb1bf416854f0c47263b9e9
helisson <helissonomc@dcomp.ufs.br>
2021-12-08 16:33:20 -0300

    making creds more secure

--- app/app/settings.py
+++ app/app/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d0nvu$3i$6)5$2i311vpnu5esp%9+5_98%ih)ja=h9az$a^-0_'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
